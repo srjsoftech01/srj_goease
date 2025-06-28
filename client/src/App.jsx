9301414f797c624db796d2941a631bf75531ba87
SRJ softechsolutions <srjsoftechsolutions@gmail.com>
2025-06-28 16:08:47 +0530

    frontend initiated

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+   <h1 class="text-3xl text-red-500 bg-blue-200 font-bold underline">
+      Hello world!
+    </h1>  
+  
+    </>
+  )
+}
+
+export default App
